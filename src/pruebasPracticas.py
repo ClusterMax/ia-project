d9724d168253dcf94fabe118e1aeb0cab1c64d43
BitzKort <bitzkort@gmail.com>
2023-10-26 14:57:37 -0500

    correccion e implementaicon de 8 pruebas

--- src/pruebasPracticas.py
+++ src/pruebasPracticas.py
@@ -9,130 +9,103 @@
 import Modelo.CostoUniformeRecursivo as cur
 import Vista.InterfazGrafica as GUI
 
+#PRUEBA 1  POSICIONES INICIALES DE 4,6 Y META DE 0,0
 
-
-# PRUEBA 1 TABLERO ORIGINAL, DIFERENTE POSICIONES DE META E INICIO
-tablero1a = [[1,1,3,1,1,1,1,1],
-           [1,-2,0,0,-2,0,0,1],
-           [1,0,1,1,1,0,0,1], 
-           [1,0,1,0,0,0,1,1],
-           [1,-2,1,3,1,1,1,1]]
-
-puntoInicial1 = [2,7]
-PuntoFinal = [2,0]
-
-# Define la matriz del tablero
-tablero1 = [[1, -1, 3, 1, 1, 1, 1, 1],
-           [1, 2, 0, 0, 2, 0, 0, 1],
+tablero2a = [[1, 1, 3, 1, 1, 1, 1, 1],
+           [1, -2, 0, 0, -2, 0, 0, 1],
            [1, 0, 1, 1, 1, 0, 0, 1],
-           [1, 0, 1, 0, 0, 0, 1, 1],
-           [1, 2, 1, 3, 1, 1, 4, 1]]
+           [1, 0, 1, 0, 0, 0, 3, 1],
+           [1, -2, 1, 3, 1, 1, 1, 1]]
 
 
-#algoritmo = cur.AlgoritmoCURecursivo(tablero=tablero1, puntoInicial = [2,7], puntoFinal = [2,0])
 
-#print(algoritmo.costoUniformeRecursivo())
-
-
-#obj = GUI.tablero(tablero1)
-
-#obj.start()
-
-
-#PRUEBA 2 TABLERO ORIGINAL, DIFERENTE POSICIONES DE GATOS
-
-tablero1 = [[1, 1, 1, 1, 1, 1, 1, 1],
-           [1, 2, 0, 0, 2, 0, 0, 1],
-           [4, 0, 1, 3, 1, 0, 0, -1],
-           [3, 0, 1, 0, 0, 0, 1, 1],
-           [1, 2, 1, 3, 1, 1, 1, 1]]
-
-#obj = GUI.tablero(tablero1)
-
-#obj.start()
-
-#PRUEBA 3 TABLERO ORIGINAL, DIFERENTE POSICICIONES DE AMOS
-tablero1 = [[2, 1, 1, 1, 1, 1, 1, 1],
-           [1, 1, 0, 0, 1, 0, 0, 1],
-           [4, 0, 1, 3, 1, 0, 0, -1],
-           [3, 0, 1, 0, 0, 0, 1, 1],
-           [1, 2, 1, 3, 1, 2, 1, 1]]
-
-#obj = GUI.tablero(tablero1)
-
-#obj.start()
-#PRUEBA 4 TABLERO NUEVO, POSICIONES INICIALES DE 4,6 Y META DE 0,0
-tablero2 = [[4, 1, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 3, 0, 1],
-            [1, 0, 0, 1, 0, 0, 1],
-            [1, 0, 2, 0, 2, 0, 1],
-            [1, 0, 1, 1, 0, 2, -1],
-            [1, 1, 1, 1, 1, 1, 1]]
-
-tablero2a = [[1, 1, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 3, 0, 1],
-            [1, 0, 0, 1, 0, 0, 1],
-            [1, 0, -2, 0, -2, 0, 1],
-            [1, 0, 1, 1, 0, -2, 1],
-            [1, 1, 1, 1, 1, 1, 1]]
-
-
-algoritmo = cur.AlgoritmoCURecursivo(tablero=tablero2a, puntoInicial = [4,6], puntoFinal = [0,0])
-
-print(algoritmo.costoUniformeRecursivo())
-
-obj2 = GUI.tablero(tablero2)
+obj2 = GUI.tablero(tablero2a, posJugador=[4,6], posMeta=[0,0])
 
 obj2.start()
 
-#PRUEBA 5 SEGUNDO TABLERO, AUMENTO DE GATOS
+#PRUEBA 2 AUMENTO DE GATOS
+tablero2b = [[1, 1, 3, 1, 1, 1, 1, 1],
+           [1, -2, 0, 0, -2, 0, 0, 1],
+           [1, 0, 1, 1, 1, 0, 0, 1],
+           [3, 0, 1, 0, 0, 0, 3, 1],
+           [1, -2, 1, 3, 1, 1, 1, 1]]
 
-tablero2 = [[4, 3, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 3, 0, 1],
-            [1, 0, 0, 1, 0, 0, 1],
-            [3, 0, 2, 0, 2, 0, 1],
-            [1, 0, 1, 1, 0, 2, -1],
-            [1, 1, 3, 1, 1, 1, 1]]
-
-tablero2b = [[1, 3, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 3, 0, 1],
-            [1, 0, 0, 1, 0, 0, 1],
-            [3, 0, -2, 0, -2, 0, 1],
-            [1, 0, 1, 1, 0, -2, 1],
-            [1, 1, 3, 1, 1, 1, 1]]
-
-
-algoritmo = cur.AlgoritmoCURecursivo(tablero=tablero2b, puntoInicial = [4,6], puntoFinal = [0,0])
-
-print(algoritmo.costoUniformeRecursivo())
-
-
-obj2 = GUI.tablero(tablero2)
+obj2 = GUI.tablero(tablero2b)
 
 obj2.start()
 
-#PRUEBA 6 SEGUNDO TABLERO AUMENTO DE AMOS
+#PRUEBA 3 DIFRENTE POSICIONES DE META Y JUGADOR
 
-tablero2 = [[4, 3, 1, 3, 1, 1, 1],
-            [1, 0, 0, 0, 3, 0, 1],
-            [1, 0, 0, 1, 0, 0, 1],
-            [3, 0, 2, 0, 2, 0, 1],
-            [1, 0, 1, 1, 0, 2, -1],
-            [2, 1, 3, 1, 2, 1, 1]]
-
-tablero2c = [[1, 3, 1, 3, 1, 1, 1],
-            [1, 0, 0, 0, 3, 0, 1],
-            [1, 0, 0, 1, 0, 0, 1],
-            [3, 0, -2, 0, -2, 0, 1],
-            [1, 0, 1, 1, 0, -2, 1],
-            [-2, 1, 3, 1, -2, 1, 1]]
+tablero2c = [[1, 1, 3, 1, 1, 1, 1, 1],
+           [1, -2, 0, 0, -2, 0, 0, 1],
+           [1, 0, 1, 1, 1, 0, 0, 1],
+           [3, 0, 1, 0, 0, 0, 3, 1],
+           [1, -2, 1, 3, 1, 1, 1, 1]]
 
 
-algoritmo = cur.AlgoritmoCURecursivo(tablero=tablero2c, puntoInicial = [4,6], puntoFinal = [0,0])
 
-print(algoritmo.costoUniformeRecursivo())
-
-obj2 = GUI.tablero(tablero2)
-
-
+obj2 = GUI.tablero(tablero=tablero2c, posJugador=[4,7], posMeta=[2,3])
 obj2.start()
+
+#PRUEBA 4 AUMENTO DE AMOS Y DE MUROS
+
+tablero3 = [[0, 0, 3, 1, 1, 1, 1, 1],
+           [1, -2, 0, 0, -2, 0, 0, 1],
+           [1, 0, 1, 1, 1, 0, 0, 1],
+           [3, 0, 1, 0, 0, 0, 3, 1],
+           [1, -2, 1, 3, 0, -2, 1, 1]]
+
+
+
+obj2 = GUI.tablero(tablero=tablero3, posJugador=[4,7], posMeta=[2,3])
+obj2.start()
+
+
+#PRUEBA 5 ESCENARIO DIFERENE    
+
+game_board = [[1, 0, 0, 0, 0, 0, 1, -2],
+              [3, 1, 1, 1, 1, 1, 1, 0],
+              [1, 1, 0, 1, -2, 1, -2, 0],
+              [1, -2, 0, 1, 0, 1, 1, 1],
+              [1, 1, 3, -2, 1, 3, 1, 3],
+]
+
+obj2 = GUI.tablero(tablero=game_board, posJugador=[4,0], posMeta=[1,5])
+obj2.start()
+
+
+#PRUEBA 6 ESCENARIO DIFERENTE MAS MUROS  
+
+game_board = [[1, 0, 0, 0, 0, 0, 1, -2],
+              [3, 1, 1, 1, 0, 1, 0, 0],
+              [1, 1, 0, 1, -2, 1, -2, 0],
+              [1, -2, 0, 1, 0, 1, 1, 1],
+              [1, 1, 3, -2, 1, 3, 1, 3],
+]
+
+obj2 = GUI.tablero(tablero=game_board, posJugador=[4,0], posMeta=[1,5])
+obj2.start()
+
+#PRUEBA 7 ESCENARIO MAS GRANDE 
+game_board = [ [1, 0, 0, 0, 0, 0, 1, -2, 1, 1, 1, 1],
+              [3, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1],
+              [1, 1, 0, 1, -2, 1, -2, 0, 1, 0, 1, 3],
+              [1, -2, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+              [1, 1, 3, -2, 1, 3, 1, 3, 1, 1, -2, 1],
+]
+
+obj2 = GUI.tablero(tablero=game_board, posJugador=[4,0], posMeta=[1,9])
+obj2.start()
+
+#PRUEBA 8 ESCENARIO MAS GRANDE DIFERENTES POSICIONES
+
+game_board = [ [1, 0, 0, 0, 0, 0, 1, -2, 1, 1, 1, 1],
+              [3, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1],
+              [1, 1, 0, 1, -2, 1, -2, 0, 1, 0, 1, 3],
+              [1, -2, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+              [1, 1, 3, -2, 1, 3, 1, 3, 1, 1, -2, 1],
+]
+
+obj2 = GUI.tablero(tablero=game_board, posJugador=[4,8], posMeta=[1,1])
+obj2.start()
+
